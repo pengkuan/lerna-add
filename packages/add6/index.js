967ee4d25f93f0b008d6c910876cb35fc7fafd73
pk <m13005494029@163.com>
2021-03-21 22:37:27 +0800

    update: add6 add includes

--- packages/add6/index.js
+++ packages/add6/index.js
@@ -1,7 +1,11 @@
 import commonRRR from "@pengxiaobai/common";
-
+const arr = [1, 2, 4, 6];
 export const add6 = (m, n, o) => {
   commonRRR(19);
   console.log("pk ad6");
-  return m + n + o;
+  const res = arr.includes(m);
+  if (res) {
+    return m + n + o;
+  }
+  return false;
 };
